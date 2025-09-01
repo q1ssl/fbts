d38d6d91c43849834dede7f2ec85ad4e4d54941b
daiyan52 <rehanalamkkr@gmail.com>
2025-09-01 18:17:55 +0000

    ctc

---
+++ fbts/fbts/doctype/ctc_template/ctc_template.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Urvish Sanghvi and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("CTC Template", {
+// 	refresh(frm) {
+
+// 	},
+// });
